d412cc7f8fc6b91cf2c6798bdfa0b8d19056d1df
yoonsik(for private project) <yoonsik@sin-yunsig-ui-MacBookPro.local>
2023-10-14 17:01:12 +0900

    queryDSL셋팅 테스트

---
+++ src/main/java/com/study/querydsl/entity/Hello.java
@@ -0,0 +1,16 @@
+package com.study.querydsl.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+
+@Data
+@Entity
+public class Hello {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+}
--- src/test/java/com/study/querydsl/QueryDslApplicationTests.java
+++ src/test/java/com/study/querydsl/QueryDslApplicationTests.java
@@ -1,13 +1,39 @@
 package com.study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.study.querydsl.entity.Hello;
+import com.study.querydsl.entity.QHello;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class QueryDslApplicationTests {
+
+    @PersistenceContext
+    EntityManager em;
 
     @Test
     void contextLoads() {
+        // TODO: 테스트 성공여부 질의함!! 미확인
+
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory jpaQueryFactory = new JPAQueryFactory(em);
+        QHello qHello = new QHello("hello");
+        Hello result = jpaQueryFactory
+                .selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
     }
 
 }
